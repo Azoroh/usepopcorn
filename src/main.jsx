5acbeddaaac5e66905e26d1372ef885b1eec9407
jay <cjaylambs@gmail.com>
2026-02-12 22:56:58 +0100

    building the starRating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRange={7} />
   </StrictMode>,
-)
+);
